da2c2305ad4ddaf72b05c1e7060670fef2e359a5
PhilipJPrice <philip.price.1994@gmail.com>
2022-12-10 18:23:17 -0800

    implement logging with logback and slf4j

--- src/main/java/com/mycompany/propertymanagement/dto/UserDTO.java
+++ src/main/java/com/mycompany/propertymanagement/dto/UserDTO.java
@@ -5,6 +5,10 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -12,7 +16,12 @@ import lombok.Setter;
 public class UserDTO {
     private Long id;
     private String ownerName;
+    @NotNull(message = "Email is mandatory")
+    @NotEmpty(message = "Email cannot be empty")
+    @Size(min = 1, max = 50, message = "Email should be between 1 to 50 characters")
     private String ownerEmail;
     private String phone;
+    @NotNull(message = "Password cannot be null")
+    @NotEmpty(message = "Password cannot be empty")
     private String password;
 }
